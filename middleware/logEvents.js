a8aa0623d6509c9e828a986c07ce30aa789b69a7
Gabriel Ichiro <gabriel582084@gmail.com>
2023-06-13 14:24:52 -0400

    addes cors and allowed origins

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -10,11 +10,11 @@ const logEvents = async (message) => {
   console.log(logItem);
 
   try {
-    if (!fs.existsSync(path.join(__dirname, "logs"))) {
-      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    if (!fs.existsSync(path.join(__dirname, "..", "logs"))) {
+      await fsPromises.mkdir(path.join(__dirname, "..", "logs"));
     }
     await fsPromises.appendFile(
-      path.join(__dirname, "logs", "eventLog.txt"),
+      path.join(__dirname, "..", "logs", "eventLog.txt"),
       logItem
     );
   } catch (err) {
@@ -22,4 +22,10 @@ const logEvents = async (message) => {
   }
 };
 
-module.exports = logEvents;
+const logger = (req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, "reqLog.txt");
+  console.log(`${req.method} ${req.path}`);
+  next();
+};
+
+module.exports = { logger, logEvents };
